566b4ae0f9d9870e750b8e0f75344433ee75b29c
Adebayo Mustafa <adebsalert@gmail.com>
2020-09-14 23:28:15 +0100

    reading json files

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,8 +3,35 @@ const express = require('express')
 const router = express.Router()
 const appRouter = require('./app')
 const config = require('../../config')
+var path = require('path')
 
 router.get('/status', (req, res) => { res.send({status: `${config.app} is OK`}) }) // api status
+
+router.get('/ads.json', (req, res) => {
+  console.log(res)
+
+  /* Insted of doing all this */
+  // res.writeHead(200, {
+  //    'Content-type': 'application/json'
+  // });
+  // res.end(JSON.stringify(data));
+
+  /* Just send the file */
+  res.sendFile(path.join(__dirname, '../../../', 'ads.json'))
+})
+
+router.get('/servers.json', (req, res) => {
+  console.log(res)
+
+  /* Insted of doing all this */
+  // res.writeHead(200, {
+  //    'Content-type': 'application/json'
+  // });
+  // res.end(JSON.stringify(data));
+
+  /* Just send the file */
+  res.sendFile(path.join(__dirname, '../../../', 'servers.json'))
+})
 
 router.use('/app', appRouter) // mount app paths
 
